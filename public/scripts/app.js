21a9e91c3859d532537b0e0a30ece17619d172c6
Darren Picard <darrenpicard25@gmail.com>
2019-02-20 22:48:43 +0000

    Totally forgot to commit alot of changes. Alot has changed but it is working great. Tweets now post and are stored in temp data base. You can add new tweets and they display using ajax. Css is close to on point

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+$(() => {
+  function createTweetElement(data) {
+    let $tweet = $("<article>").addClass("new-tweet");
+    let $header = $("<header>").append($('<img>').attr('alt', 'Profile Pic').attr('src', data.user.avatars.small))
+                                .append($('<h4>').text(data.user.name))
+                                .append($('<p>').text(data.user.handle));
+
+
+    let $middleDiv = $("<div>").append($('<p>').text(data.content.text));
+
+    let $footer = $("<footer>")
+                                .append($('<p>').text(data.created_at))
+                                .append($('<i>').addClass("far fa-flag"))
+                                .append($('<i>').addClass("fas fa-retweet"))
+                                .append($('<i>').addClass("fas fa-heart"));
+
+    $tweet.append($header).append($middleDiv).append($footer);
+  return $tweet;
+  }
+
+  function renderTweets(tweets) {
+    // loops through tweets
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      for (let tweetInfo of tweets) {
+        displayData = createTweetElement(tweetInfo);
+        $('#tweets-container').prepend(displayData);
+      }
+  }
+
+  function loadTweets() {
+    $.get('/tweets').then(function (tweetData) {
+      renderTweets(tweetData);
+    });
+  }
+
+  loadTweets();
+
+  $('#toggle-button').click(function() {
+    console.log('Click');
+    if ( $( "#postTweet" ).is( ":hidden" ) ) {
+      $( "#postTweet" ).show( "slow" );
+    } else {
+      $( "#postTweet" ).slideUp();
+    }
+});
+
+  $('#newTweetForm').on('submit', function() {
+    event.preventDefault();
+    let counter = $(this).children('.counter').text();
+    if (counter >= 0 && counter < 140) {
+      let data = $(this).serialize();
+      $.post('/tweets', data, function(data) {
+      $('#tweets-container').prepend(createTweetElement(data));
+
+      });
+    } else if (counter < 0) {
+      alert('Message is to Long');
+    } else {
+      alert('No message to Tweet');
+    }
+  });
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+//waiting for the entire dom is ready and loaded
+$(() => {
+
+  //Counts number of characters subtracts max-length by this and puts into span tag
+  $('.new-tweet form textarea').keyup(function () {
+    let max_length = 140;
+    let remaining_length = max_length - $(this).val().length;
+    $(this).siblings('.counter').text(remaining_length);
+    if (remaining_length < 0) {
+      $(this).siblings('.counter').css('color', 'red');
+    } else {
+      $(this).siblings('.counter').css('color', 'grey');
+    }
+  });
+});
